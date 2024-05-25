613f128e597b29299078c37c43357891f844b150
saileshghimire <saileshghimire1@gmail.com>
2024-05-25 08:17:50 +0545

    todo function

---
+++ react-todo/src/Todos.jsx
@@ -0,0 +1,13 @@
+export function Todo({title, description}){
+    return(
+        <>
+        <h3>
+            {title}
+        </h3>
+        <br />
+        <h5>
+            {description}
+        </h5>
+        </>
+    )
+}
