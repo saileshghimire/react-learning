8e0f7d34fd78ff1d8d9435f5ddf681c115e938b2
saileshghimire <saileshghimire1@gmail.com>
2024-05-25 08:24:51 +0545

    todo listing

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {} from './Todos'
+import { Todo} from './Todos'
 
 function App() {
-  const [todos, setTodos] = useState(0)
+  const [todos, setTodos] = useState([{
+    id:1,
+    title:"Gym",
+    description:"Go to the Gym everyday expect saturday"
+  },
+{
+  id:2,
+  title:"Code",
+  description:"Do coding 8 hours a day"
+},
+{
+  id:4,
+  title:"Code-Code",
+  description:"Do coding 8 hours a day"
+},{
+  id:5,
+  title:"Coding",
+  description:"Do coding 8 hours a day"
+}
+])
 
   return (
     <>
-      
+      <Todo></Todo>
     </>
   )
 }
