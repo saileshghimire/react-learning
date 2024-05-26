27be0a68d0215010960ef82b047cab9fe1af686b
saileshghimire <saileshghimire1@gmail.com>
2024-05-26 21:28:49 +0545

    use memo

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
+import { number } from 'zod';
 // import { Todos } from "./Todo"
 
 
@@ -49,31 +50,60 @@ function Todo({id}){
   )
 }
 
-function App(){
-  const [todoID, setTodoId] = useState(1);
+// get todo specific todo by id using button
 
-function clickHander(id){
-  setTodoId(id);
-}
-  return(
-    <>
-    <button onClick={function(){
-      setTodoId(1)
-    }}>1</button>
-    <button onClick={function(){
-      setTodoId(2)
-    }}>2</button>
-    <button onClick={function(){
-      setTodoId(3)
-    }}>3</button>
-    <button onClick={() => clickHander(4)}>4</button>
-    <Todo id ={todoID} />
-   </>
+// function App(){
+//   const [todoID, setTodoId] = useState(1);
 
-  //  <>
-  //   <Todo id ={3}></Todo>
-  //   </>
-  )
-}
+// function clickHander(id){
+//   setTodoId(id);
+// }
+//   return(
+//     <>
+//     <button onClick={function(){
+//       setTodoId(1)
+//     }}>1</button>
+//     <button onClick={function(){
+//       setTodoId(2)
+//     }}>2</button>
+//     <button onClick={function(){
+//       setTodoId(3)
+//     }}>3</button>
+//     <button onClick={() => clickHander(4)}>4</button>
+//     <Todo id ={todoID} />
+//    </>
+//   )
+// }
+
+// useMemo
+// function App(){
+
+//   const[count, setCount] = useState(0);
+//   const [number, setsum] = useState(0);
+
+//   function Counter(){
+//     setCount(count + 1);
+//   }
+
+//   function numberSum(num){ 
+//     let n = parseInt(num);
+//     setsum((n * (n + 1)) / 2);
+//   }
+
+//   return(
+//     <>
+//     <input type="text" onChange={e => {
+//      numberSum(e.target.value)
+//     }} />
+//     <br />
+//     Sum is {number}
+//     <br />
+//     <br />
+//     <button onClick={Counter}>Counter ({count})</button>
+//     </>
+//   )
+// }
+
+
 
 export default App
