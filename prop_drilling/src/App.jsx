ddf350ca41fec110adfcc1b8bad27b5d079f401b
saileshghimire <saileshghimire1@gmail.com>
2024-05-28 21:36:05 +0545

    context-api

--- prop_drilling/src/App.jsx
+++ prop_drilling/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+function App(){
+  const [count, setCount] = useState(0);
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <CountContext.Provider value={count}>
+    <Count setCount={setCount} />
+    </CountContext.Provider>
     </>
   )
+
+
+}
+
+
+function Count({count, setCount}){
+  return(
+    <>
+    <CountRender />
+    <Button setCount={setCount} />
+    </>
+  );
+}
+
+function CountRender(){
+  const count = useContext(CountContext);
+  return(
+    <>
+    {count}
+    </>
+  )
+}
+
+function Button({setCount}){
+  const count = useContext(CountContext);
+  return(
+  <>
+  <button onClick={() =>{
+    setCount(count +1);
+  }}>Increase</button>
+  
+  <button onClick={() =>{
+    setCount(count -1)
+  }}>Descrease</button>
+  </>
+  );
 }
 
 export default App
