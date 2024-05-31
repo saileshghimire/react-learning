42c61b877a3e6d3c69d6bf7f69be7b1d2ec6500d
saileshghimire <saileshghimire1@gmail.com>
2024-05-31 14:31:20 +0545

    creation of atom

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -2,32 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return(
+    <>
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+    </>
+  )
+}
+
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsNotificationCount = useRecoilValue(jobsAtom);
+  const messageNotificationCount  = useRecoilValue(messagingAtom);
+  const notificationNotificationCount = useRecoilValue(notificationAtom);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button>Home</button>
+    <button>My network({(networkNotificationCount >= 100) ?"99+": networkNotificationCount})</button>
+    <button>Jobs ({(jobsNotificationCount >=100)?"99+":jobsNotificationCount})</button>
+    <button>Messages ({(messageNotificationCount >=100)?"99+":messageNotificationCount})</button>
+    <button>Notification({(notificationNotificationCount >=100)?"99+":notificationNotificationCount})</button>
     </>
   )
 }
