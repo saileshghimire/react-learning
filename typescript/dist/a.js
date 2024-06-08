7ab24a243e04db272ea28d1fbe11051082cf10f6
saileshghimire <saileshghimire1@gmail.com>
2024-06-08 15:11:31 +0545

    interface

--- typescript/dist/a.js
+++ typescript/dist/a.js
@@ -1,11 +1,14 @@
 "use strict";
-// function sumnumber(a:number,b:number):number {
-//     return a+b;
-// }
-// passing a function as paramter.
 function runAfter1s(fn) {
     setTimeout(fn, 1000);
 }
 runAfter1s(function () {
     console.log("hi there");
 });
+function islegal(user) {
+    if (user.age > 18) {
+        return true;
+    }
+    else
+        return false;
+}
--- typescript/scr/a.ts
+++ typescript/scr/a.ts
@@ -12,3 +12,18 @@ runAfter1s(function(){
     console.log("hi there");
     
 });
+
+interface User {
+    name:"sailesh",
+    lastName:"Ghimire",
+    age:21
+}
+
+function islegal(user:User){
+    if (user.age>18){
+        return true;
+
+    }
+    else return false
+}
+
